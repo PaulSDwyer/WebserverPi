a1e498644244cc8c5f05b91af8f32cf7d562fd60
Paul Dwyer <Doominator25@gmail.com>
2019-01-14 15:43:23 +1300

    Update webserverPi.js

--- webserverPi.js
+++ webserverPi.js
@@ -2,8 +2,10 @@ var http = require('http');
 var port = 8080;
 
 http.createServer(function(req, res) {
+	console.log(req);
 	res.write('Hello World!');
 	res.end();
 }).listen(port);
 
 console.log("Server listening on port " + port);
+console.log("Press Ctrl+C to exit.");
